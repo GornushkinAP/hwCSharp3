1a40cffb776410eab6e3234baa4ec041de89b897
GornushkinAP <gapEdu@yandex.ru>
2023-07-06 16:10:41 +0300

    Добавил все папки и файлы в локальный репозиторий

---
+++ task19Polindrom/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("И Снова Здравствуйте! Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (IsPalindrome(number))
+    {
+     Console.WriteLine("Данное число является палиндромом. Всего доброго. ");
+    }
+    else
+    {
+     Console.WriteLine("Данное число не является палиндромом. Всего доброго. ");
+    }
+
+static bool IsPalindrome(int number)
+{
+    string numberString = number.ToString();
+    int length = numberString.Length;
+
+    for (int i = 0; i < length / 2; i++)
+    {
+        if (numberString[i] != numberString[length - i - 1])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
---
+++ task23/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу 
+// кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Таблица кубов чисел от 1 до " + n + ":");
+Console.WriteLine("Число\tКуб");
+
+for (int i = 1; i <= n; i++)
+    {
+    int cube = i * i * i;
+    Console.WriteLine(i + "\t" + cube);
+    }
+Console.ReadLine();
